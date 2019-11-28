2a0130af6fa844612c83f1bc35b38ee70dd6ffdf
lienharj <jolienhart8@gmail.com>
2019-11-28 18:29:09 +0100

    adustable gain through env

--- launch.sh
+++ launch.sh
@@ -5,4 +5,6 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 rosservice call /$VEHICLE_NAME/led_emitter_node/set_custom_pattern "pattern: {color_list: ['white','red','switchedoff','red','white'], frequency: 0.0}"
+[ ! -z "$GAIN" ] && rosparam set /$VEHICLE_NAME/kinematics_node/gain $GAIN
+
 roslaunch duckietown_demos lane_following_min.launch veh:=$VEHICLE_NAME
